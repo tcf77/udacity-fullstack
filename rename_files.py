91174b5e088d72ef37777d4e1b562ecef6b9e5d5
JoeTj <joekic@hotmail.com>
2016-08-10 14:05:21 -0400

    added code to rename files, graphics tool to draw picture

--- rename_files.py
+++ rename_files.py
@@ -2,13 +2,14 @@ import os
 
 def rename_files():
     #(1) get file names from a folder
-    file_list = os.listdir(r"D:\Practice\Python-Udacity\prank\prank")
+    file_list = os.listdir(r"D:\Udacity\Fullstack\prank\prank")
     print(file_list)
     saved_path = os.getcwd()
-    print("Current Working Directory is " + saved_path)      
+    print("Current Working Directory is " + saved_path)
+    os.chdir(r"D:\Udacity\Fullstack\prank\prank")
 
     #(2) for each file, rename filename
-    #for file_name in file_list:
-        #os.rename(file_name, file_name.translate(None, "0123456789"))
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
         
 rename_files()
